43434791674e38d17ffde0c005e35a4680bf2560
lqzph <654980002@qq.com>
2016-12-05 10:38:32 +0800

    news

--- js/index.js
+++ js/index.js
@@ -1,13 +1,7 @@
-window.onload=function () {
-   var nav=document.getElementsByClassName("nav")[0];
-    var na=document.getElementsByTagName("nav","a");
-    na.addEventListener("touchstart",function (e) {
-        var ev=e || window.event;
-        var e=ev.target ||ev.srcElement;
-        for(i=0;i<na.length;i++){
-          na[i].removeClass("active")
-            e.addClass("active")
-        }
-    })
 
-}
+// $(function () {
+//     $(".nav a").click(function () {
+//         var index=$(this).index();
+//        $(".nav a").removeClass("active").eq(index).addClass("active");
+//     })
+// })
