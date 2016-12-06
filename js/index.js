cf051cb4a2bcf25aa4b04515cbfbedbbe4399505
lqzph <654980002@qq.com>
2016-12-06 10:02:31 +0800

    as

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 
-// $(function () {
-//     $(".nav a").click(function () {
-//         var index=$(this).index();
-//        $(".nav a").removeClass("active").eq(index).addClass("active");
-//     })
-// })
+$(function () {
+    var flag=true;
+$(".jia").click(function () {
+    if(flag==true){
+        $(".xiala").css({animation:"xia 2s forwards"})
+        flag=false;
+    }else{
+        $(".xiala").css({animation:"xia1 2s forwards"})
+        flag=true;
+    }
+})
+})
